54f96be2eef6224d53b99112a05436cda137ed53
jihwangim09-design <jihwangim09@gmail.com>
2026-07-08 15:34:54 +0900

    <

--- day02/practice1.js
+++ day02/practice1.js
@@ -1,3 +1,9 @@
+
+/*
+
+문제1
+손님에게 이름과 원하는 음료, 그리고 수량을 순서대로 물어본 후, 모든 정보를 조합하여 
+바리스타가 볼 수 있도록 콘솔에 주문 내역을 출력하는 프로그램을 만드세요.
 console.log("[주문 접수]");
 
 alert("안녕하세요 손님의 성함을 입력해주세요!");
@@ -19,3 +25,5 @@ document.write("<h1>주문 정보</h1>");
 document.write("이름 : " + username + "<br />");
 document.write("메뉴 : " + foodname + "<br />");
 document.write("수량 : " + quantity + "<br />");
+
+*/
