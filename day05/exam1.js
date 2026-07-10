25fbb342e3d2705319d44b981c67dbaf0b8d3dd0
jihwangim09-design <jihwangim09@gmail.com>
2026-07-10 11:45:17 +0900

    >

---
+++ day05/exam1.js
@@ -0,0 +1,11 @@
+//1부터 50까지 합을 구하여 출력
+for (let number = 1 ; number<=50 ; number++){
+    sum+=number
+}
+(console.log( sum ) )
+
+//1부터 20까지 숫자 중 짝수만 출력
+for (let number = 1 ; number<=20 ; number++) {
+    if ( number % 2 == 0) {console.log (number) }
+    else    
+}
