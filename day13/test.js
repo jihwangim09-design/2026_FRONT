88213819387ad83e3bf8068904c3e5fb6ed107c1
jihwangim09-design <jihwangim09@gmail.com>
2026-07-23 16:52:46 +0900

    >

---
+++ day13/test.js
@@ -0,0 +1,23 @@
+function boardwrite() {
+    const titleinput = document.querySelector('#title')
+    const contentinput = document.querySelector('#content')
+    const pwinput = document.querySelector('#pw')
+
+    const title = titleinput.value
+    const content = contentinput.value
+    const pw = pwinput.value
+
+    let obj = { title, content, pw}
+
+    let boardList = localStorage.getItem('boardList')
+    if (boardList == null) (boardList = [])
+        else {boardList = JSON.parse(boardList)}
+
+    obj.no = boardList.length == 0 ? 1 : boardList[boardList.length - 1].no + 1
+    boardList.push(obj)
+
+    localStorage.setItem('boredList' , JSON.stringify(boardList))
+
+    alert('게시물 등록 성공!')
+}
+
